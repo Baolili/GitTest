f9409e20cf9f5901856160d98a80687f8dcdbac5
baolili <1977311963@qq.com>
2019-07-02 15:32:34 +0800

    提交目录

--- src/main/java/entity/HelloWorld.java
+++ src/main/java/entity/HelloWorld.java
@@ -1,7 +1,9 @@
 package entity;
 public class HelloWorld {
     public static void main(String[] args) {
-
+        System.out.println("h");
         System.out.println("ok");
+
+        System.out.println("okss");
     }
 }
